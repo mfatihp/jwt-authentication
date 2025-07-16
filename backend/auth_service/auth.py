6fd1bd2bbd185b06ab2ee6172196abf9482a4ed4
mfatihp <mfatihp@users.noreply.github.com>
2025-07-16 17:00:12 +0300

    add data service into structure and add token endpoint

--- backend/auth_service/auth.py
+++ backend/auth_service/auth.py
@@ -1,21 +1,41 @@
-from fastapi import APIRouter, Depends
-from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.context import CryptContext
+
+# Custom modules
+from utils.tokens import Token
+from utils.tmp_db import users_db # Temp test db, will be replaced with postgresql
 
 
-
-router = APIRouter()
+app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-# DB
-# Classes maybe
-users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+def validate_user(username, password):
+    # Query user hashed password
+    if username in users_db:
+        user_dict = users_db[username]
+        hashed_pwd = user_dict["hashed_password"]
+
+        # Verify password
+        matched = pwd_context.verify(password, hashed_pwd)
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None): # data : {"sub": user.username} 
+
+    return None
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    auth = validate_user(form_data.username, form_data.password)
+
+    if auth:
+        access_token_expires = timedelta(minutes=15)
+        access_token = create_access_token()
+
+        return Token(access_token=access_token, token_type="bearer")
