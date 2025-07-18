7b9b61dd2202b622841ede266c50c54f80d30507
mfatihp <mfatihp@users.noreply.github.com>
2025-07-18 17:14:27 +0300

    implement JWT  token create function

--- backend/auth_service/auth.py
+++ backend/auth_service/auth.py
@@ -1,19 +1,31 @@
+from dotenv import load_dotenv
 from datetime import datetime, timedelta, timezone
+from zoneinfo import ZoneInfo
 from typing import Annotated
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from passlib.context import CryptContext
+import jwt
+import os
+import uvicorn
+
 
 # Custom modules
 from utils.tokens import Token
 from utils.tmp_db import users_db # Temp test db, will be replaced with postgresql
 
+load_dotenv()
+
 
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM")
+
 
 def validate_user(username, password):
     # Query user hashed password
@@ -23,11 +35,23 @@ def validate_user(username, password):
 
         # Verify password
         matched = pwd_context.verify(password, hashed_pwd)
+    else: 
+        matched = False
+    return matched
+
+
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None): # data : {"sub": user.username} 
+    to_encode = data.copy()
+    expire = datetime.now(ZoneInfo("Europe/Istanbul")) + expires_delta
+    print(expire)
+    to_encode.update({"exp": expire})
 
-    return None
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
 
 
 @app.post("/token")
@@ -36,6 +60,15 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
 
     if auth:
         access_token_expires = timedelta(minutes=15)
-        access_token = create_access_token()
+        access_token = create_access_token(data = {"sub": form_data.username}, expires_delta=access_token_expires)
 
         return Token(access_token=access_token, token_type="bearer")
+    
+    return "Success"
+    
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
